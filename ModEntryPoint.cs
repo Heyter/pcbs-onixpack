18fa176c2bbd1274bb7ca492335415300e5972e8
Sam Nyashev <12237569+Heyter@users.noreply.github.com>
2021-01-05 20:42:51 +0300

    1.0.1

--- ModEntryPoint.cs
+++ ModEntryPoint.cs
@@ -1,26 +1,24 @@
 ﻿using System;
 using BepInEx;
-using BepInEx.Logging;
 using BepInEx.Configuration;
 using HarmonyLib;
 using UnityEngine;
 using UnityEngine.PostProcessing;
-using cakeslice;
 
 namespace OnixPack
 {
-    [BepInPlugin(MOD_GUID, MOD_NAME, "1.0")]
-    public class ModEntryPoint : BaseUnityPlugin
-    {
-        public const string MOD_NAME = "OnixPack";
-        public const string MOD_GUID = "org.bepinex.plugins." + MOD_NAME;
+	[BepInPlugin(MOD_GUID, MOD_NAME, "1.0.1")]
+	public class ModEntryPoint : BaseUnityPlugin
+	{
+		public const string MOD_NAME = "OnixPack";
+		public const string MOD_GUID = "org.bepinex.plugins." + MOD_NAME;
 		internal void Awake()
 		{
 			instant3DMark = base.Config.Bind("General", "Instant3DMark", true, "Instant 3DMark");
 
 			try
-            {
-                if (FindObjectsOfType<ModEntryPoint>().Length > 1)
+			{
+				if (FindObjectsOfType<ModEntryPoint>().Length > 1)
 				{
 					Debug.Log(string.Format("[{0}] Another instance of {1} was instantiated. Will destroy this: {2}", MOD_NAME, typeof(ModEntryPoint), gameObject.GetInstanceID()));
 					DestroyImmediate(this);
@@ -38,25 +36,26 @@ namespace OnixPack
 		}
 
 		private void Update()
-        {
+		{
 			if (fpsBoost && Time.time > fpsBoostTime)
 			{
 				fpsBoostTime += 3;
 
-				foreach (Camera camera in Camera.allCameras)
-				{
+				for (int i = 0; i < Camera.allCamerasCount; i++)
+                {
+                    Camera camera = Camera.allCameras[i];
 					camera.gameObject.GetComponent<PostProcessingBehaviour>().enabled = false;
-					camera.gameObject.GetComponent<HBAOControl>().hbao.enabled = false;
 					camera.gameObject.GetComponent<HBAOControl>().enabled = false;
-					camera.gameObject.GetComponent<FxaaComponent>().OnDisable();
-					camera.gameObject.GetComponent<AmbientOcclusionComponent>().model.enabled = false;
-					camera.gameObject.GetComponent<AmbientOcclusionModel>().enabled = false;
-					camera.gameObject.GetComponent<Camera>().allowHDR = false;
-					camera.gameObject.GetComponent<DustVolume>().enabled = false;
-					camera.gameObject.GetComponent<RealIllumination>().enabled = false;
-					camera.gameObject.GetComponent<ReflectionProbe>().enabled = false;
-				}
-			}
+                    camera.gameObject.GetComponent<FxaaComponent>().OnDisable();
+                    camera.gameObject.GetComponent<AmbientOcclusionComponent>().model.enabled = false;
+                    camera.gameObject.GetComponent<AmbientOcclusionModel>().enabled = false;
+                    camera.gameObject.GetComponent<Camera>().allowHDR = false;
+                    camera.gameObject.GetComponent<Camera>().allowMSAA = false;
+                    camera.gameObject.GetComponent<DustVolume>().enabled = false;
+                    camera.gameObject.GetComponent<RealIllumination>().enabled = false;
+                    camera.gameObject.GetComponent<ReflectionProbe>().enabled = false;
+                }
+            }
 
 			if (Input.GetKey(KeyCode.F11))
 			{
@@ -67,26 +66,32 @@ namespace OnixPack
 				QualitySettings.shadowDistance = 0f;
 				QualitySettings.shadowCascades = 0;
 				QualitySettings.pixelLightCount = 0;
-				QualitySettings.resolutionScalingFixedDPIFactor = 0.5f;
 				QualitySettings.vSyncCount = 0;
 				QualitySettings.blendWeights = (BlendWeights)0;
 				QualitySettings.lodBias = 0.3f;
-				QualitySettings.streamingMipmapsActive = false;
-				OutlineEffect.Instance.lineThickness = 0.5f;
+				QualitySettings.resolutionScalingFixedDPIFactor = 0.5f;
+				QualitySettings.maxQueuedFrames = 0;
+				// TODO: cakeslice.OutlineEffect eats a lot of FPS.
+				/*OutlineEffect.Instance.lineThickness = 0.5f;
 				OutlineEffect.Instance.lineIntensity = 0.05f;
-				OutlineEffect.Instance.fillAmount = 0.05f;
+				OutlineEffect.Instance.fillAmount = 0.05f;*/
 				QualitySettings.particleRaycastBudget = 0;
 				QualitySettings.softParticles = false;
 				QualitySettings.maximumLODLevel = 0;
 				QualitySettings.softVegetation = false;
-				QualitySettings.SetQualityLevel(0);
 				GameController.Get().IsLightsOn = false;
 				PlayerPrefs.SetInt("AmbientOcclusion", 0);
 				PlayerPrefs.SetInt("CameraEffects", 0);
 				PlayerPrefs.SetInt("MotionBlur", 0);
 
+				Resolution resolution = Screen.currentResolution;
+				Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen, resolution.refreshRate);
+
+				QualitySettings.SetQualityLevel(0, true);
+
 				fpsBoost = true;
 			}
+
 		}
 
 		public static ConfigEntry<bool> instant3DMark;
